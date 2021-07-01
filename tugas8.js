61ff0255c33aaaad640a7ca4c53c8c065e84836c
Decky Fajar Sidiq <46802206+deckyfajars@users.noreply.github.com>
2021-07-01 17:06:54 +0700

    Add files via upload

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+function objek(){
+  var berita = {
+    kota : "Bandung",
+    status : "Waspada",
+    wilayah : "kabupaten",
+  }
+  berita.kota = "Jakarta";
+  console.log(berita);
+}
+
+objek();
